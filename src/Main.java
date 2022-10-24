9ee7bfc58058a01fa1698b23702038a0ee9cdb09
pawelb92 <pawel.brzezinski92@gmail.com>
2022-10-24 20:38:10 +0200

    Dodalem ila mam lat

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String name="Adam";
-        System.out.println("Hello "+name);
+        String name="Pawel";
+        int wiek = 31;
+        System.out.println("Hello "+name+ " mam " + wiek +" lat.");
+
     }
 }
