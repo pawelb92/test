c01a5e99175d2da436e90a033b6ce0ec2b8758e6
pawelb92 <pawel.brzezinski92@gmail.com>
2022-10-24 20:41:51 +0200

    Dodalem ila mam lat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc=new Scanner( )
         String name="Pawel";
         int wiek = 31;
         System.out.println("Hello "+name+ " mam " + wiek +" lat.");
