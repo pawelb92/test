6befe07ab288816224d0ae1bf00497f4277a085f
pawelb92 <pawel.brzezinski92@gmail.com>
2022-10-24 21:16:05 +0200

    zmiana spacji

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Podaj pierwszą wartość:");
+        System.out.println("Podaj pierwszą wartość :");
         Scanner sc=new Scanner(System.in);
-        int a = sc.nextInt();
-        System.out.println("Podaj drugą wartość:");
+        int v = sc.nextInt();
+        System.out.println("Podaj drugą wartość :");
         int b = sc.nextInt();
         System.out.println("Wynik to : ");
-        System.out.println( a + b);
+        System.out.println( v+ b);
 
     }
 }
